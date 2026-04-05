ff222f30796a303705630b39e84cfd75182438a4
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-05 14:47:00 +0600

    (add & modify): theme-provider added and globas css modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -58,6 +71,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button onClick={toggleTheme}>Toggle Theme</Button>
         </div>
       </main>
     </div>
---
+++ src/components/providers/theme-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  { ssr: false },
+);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
