e87727b56d2a789b6451628118c0dbac8a72b2db
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-06 13:11:52 +0600

    metadata added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import HomePage from "@/components/pages/home/home";
 
+export const metadata = {
+  title: "Home",
+  description:
+    "A step-by-step system to eliminate procrastination, train your brain for deep work, and boost productivity effortlessly.",
+};
+
 const Home = () => {
   return (
     <>
