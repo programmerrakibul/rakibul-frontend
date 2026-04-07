3c98394ed02275ec7ce7def33c3f883585a97176
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-07 21:02:01 +0600

    (modify): separator

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -14,7 +14,7 @@ function Separator({
       data-slot="separator"
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-linear-to-r from-background via-primary/80 to-background data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        "shrink-0 bg-linear-to-r from-transparent via-primary/80 to-transparent data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className,
       )}
       {...props}
